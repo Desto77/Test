1b668a286ae76adab4b776335b5006908a603509
Flemming, Grzegorz <grzegorz.flemming@gft.com>
2015-11-23 10:21:34 +0100

    Added method test1

--- src/com/flemming/HelloWorld.java
+++ src/com/flemming/HelloWorld.java
@@ -3,5 +3,11 @@ package com.flemming;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		HelloWorld hw = new HelloWorld();
+		hw.test1();
+	}
+	
+	public void test1() {
+		System.out.println("TEST1");
 	}
 }
