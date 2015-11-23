093eebf94ef9a3a067d22de7b811bf05ddf400c6
Flemming, Grzegorz <grzegorz.flemming@gft.com>
2015-11-23 11:59:28 +0100

    Removing test methods

--- src/com/flemming/HelloWorld.java
+++ src/com/flemming/HelloWorld.java
@@ -3,24 +3,5 @@ package com.flemming;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		HelloWorld hw = new HelloWorld();
-		hw.test1();
 	}
-
-	public void test4() {
-		System.out.println("TEST4");
-	}
-	
-	public void test3() {
-		System.out.println("TEST3");
-	}
-	
-	public void test2() {
-		System.out.println("TEST2");
-	}
-
-	public void test1() {
-		System.out.println("TEST1");
-	}
-
 }
