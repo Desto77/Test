2612729265d14439007878003839d8e627fa5f2b
Flemming, Grzegorz <grzegorz.flemming@gft.com>
2015-11-23 16:42:02 +0100

    u

--- src/com/flemming/HelloWorld.java
+++ src/com/flemming/HelloWorld.java
@@ -2,13 +2,7 @@ package com.flemming;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello with fix");
-		System.out.println("funcitonality");
-	}
-
-	public void function1() {
-		System.out.println("function_1");
-		System.out.println("functionality");
+		System.out.println("Hello");
 	}
 
 }
