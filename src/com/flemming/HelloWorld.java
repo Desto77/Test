ef24efec2b82b2e83b51e8068c75467ef3e6bd1f
Flemming, Grzegorz <grzegorz.flemming@gft.com>
2015-11-23 16:46:41 +0100

    fds

--- src/com/flemming/HelloWorld.java
+++ src/com/flemming/HelloWorld.java
@@ -2,7 +2,8 @@ package com.flemming;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Adam");
+		//fdsfdsfdsfsd
 	}
 
 }
