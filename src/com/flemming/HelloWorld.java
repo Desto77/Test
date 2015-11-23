0803c7cf5b19b7758b3868f7f172d79b2e1b6968
Flemming, Grzegorz <grzegorz.flemming@gft.com>
2015-11-23 16:36:33 +0100

    fsd

--- src/com/flemming/HelloWorld.java
+++ src/com/flemming/HelloWorld.java
@@ -2,7 +2,7 @@ package com.flemming;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello with fix");
 		System.out.println("funcitonality");
 	}
 
