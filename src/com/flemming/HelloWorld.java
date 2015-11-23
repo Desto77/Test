79d57fb3544ce2f854c68ffb33a67000ec571181
Flemming, Grzegorz <grzegorz.flemming@gft.com>
2015-11-23 16:33:45 +0100

    dsa

--- src/com/flemming/HelloWorld.java
+++ src/com/flemming/HelloWorld.java
@@ -2,17 +2,7 @@ package com.flemming;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello with hot fix");
-		//hotfix to main function
+		System.out.println("Hello");
 	}
-	
-	public void function1() {
-		//this is some hotfix to function1
-	}
-	
-	public void function2() {
-		System.out.println("function2");
-		System.out.println("some functionality");
-		System.out.println("another functionality");
-	}
+
 }
